a687ca7dbca9e2cbc1a38bf5ff44e1fa2d37e2cf
Chris DeLashmutt <cdelashmutt@pivotal.io>
2019-10-01 18:36:35 +0000

    Fixing script path detection and fixing first replacement

--- apply-cc-jobs.sh
+++ apply-cc-jobs.sh
@@ -16,14 +16,15 @@ else
 
   DEPLOYED_YAML=$1
   STAGED_YAML=$2
+  SCRIPT_PATH=$(dirname $0)
 
   echo "cloud_controller_yaml:" > $TEMP_VARS_FILE
-  sed 's/^/  /' <(bosh int --path=/instance_groups/name=cloud_controller cf.yaml) >> $TEMP_VARS_FILE
+  sed 's/^/  /' <(bosh int --path=/instance_groups/name=cloud_controller $1) >> $TEMP_VARS_FILE
   echo "clock_global_yaml:" >> $TEMP_VARS_FILE
   sed 's/^/  /' <(bosh int --path=/instance_groups/name=clock_global $1) >> $TEMP_VARS_FILE
   echo "cloud_controller_worker_yaml:" >> $TEMP_VARS_FILE
   sed 's/^/  /' <(bosh int --path=/instance_groups/name=cloud_controller_worker $1) >> $TEMP_VARS_FILE
 
-  bosh int -l $TEMP_VARS_FILE -o replace-cloud-controller.yaml $2 > $3
+  bosh int -l $TEMP_VARS_FILE -o $SCRIPT_PATH/replace-cloud-controller.yaml $2 > $3
 
 fi
