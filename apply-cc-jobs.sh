3840fe1c499483020775904c7517b57e8eb3a4d9
Chris DeLashmutt <cdelashmutt@pivotal.io>
2019-10-01 18:30:16 +0000

    Merge actual YAML instead of test

--- apply-cc-jobs.sh
+++ apply-cc-jobs.sh
@@ -7,13 +7,23 @@ if [ $# -ne 3 ] || [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
     echo "Usage: $0 deployed_yaml_filename staged_yaml_filename merged_yaml_filename"
     exit 1
 else
-  
+
+  # temp file to store vars
+  TEMP_VARS_FILE=$(mktemp /tmp/apply-cc-jobs-script.XXXXXX)
+
+  # remove vars file on exit
+  trap "{ rm -f $TEMP_VARS_FILE; }" EXIT  
+
   DEPLOYED_YAML=$1
   STAGED_YAML=$2
 
-  bosh int --var-file=cloud_controller_yaml=<(bosh int --path=/instance_groups/name=cloud_controller $1) \
-    --var-file=clock_global_yaml=<(bosh int --path=/instance_groups/name=clock_global $1) \
-    --var-file=cloud_controller_worker_yaml=<(bosh int --path=/instance_groups/name=cloud_controller_worker $1) \
-    -o replace-cloud-controller.yaml $2 > $3
+  echo "cloud_controller_yaml:" > $TEMP_VARS_FILE
+  sed 's/^/  /' <(bosh int --path=/instance_groups/name=cloud_controller cf.yaml) >> $TEMP_VARS_FILE
+  echo "clock_global_yaml:" >> $TEMP_VARS_FILE
+  sed 's/^/  /' <(bosh int --path=/instance_groups/name=clock_global $1) >> $TEMP_VARS_FILE
+  echo "cloud_controller_worker_yaml:" >> $TEMP_VARS_FILE
+  sed 's/^/  /' <(bosh int --path=/instance_groups/name=cloud_controller_worker $1) >> $TEMP_VARS_FILE
+
+  bosh int -l $TEMP_VARS_FILE -o replace-cloud-controller.yaml $2 > $3
 
 fi
